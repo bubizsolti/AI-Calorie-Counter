d631cde07a6b17895afd895852566b43794b47b7
Bubenkó Zsolt <bubizsolti@mailbox.unideb.hu>
2025-03-26 13:54:04 +0100

    Picture taking is now possible, has issues

--- app/src/main/java/com/example/aikaloriaszamlalo/CameraUtils.kt
+++ app/src/main/java/com/example/aikaloriaszamlalo/CameraUtils.kt
@@ -7,11 +7,17 @@ import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.ImageCaptureException
+import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,9 +26,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.navigation.NavController
@@ -49,19 +57,24 @@ fun CameraScreen(modifier: Modifier = Modifier, navController: NavController) {
     LaunchedEffect(key1 = true) {
         launcher.launch(Manifest.permission.CAMERA)
     }
-    Column(modifier = modifier.fillMaxSize()) {
+    var imageCapture: ImageCapture? = null
+    var isPhotoTaken by remember { mutableStateOf(false) }
+    val cameraExecutor = Executors.newSingleThreadExecutor() // Define here
+
+    Column(modifier = modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
         if (hasCamPermission) {
             AndroidView(
                 factory = { context ->
                     val previewView = PreviewView(context).apply {
                         this.scaleType = PreviewView.ScaleType.FILL_CENTER
                     }
-                    val cameraExecutor = Executors.newSingleThreadExecutor()
                     val cameraProviderFuture = ProcessCameraProvider.getInstance(context)
                     cameraProviderFuture.addListener({
                         val cameraProvider = cameraProviderFuture.get()
-                        val preview = Preview.Builder().build()
-                        preview.setSurfaceProvider(previewView.surfaceProvider)
+                        val preview = Preview.Builder().build().also {
+                            it.setSurfaceProvider(previewView.surfaceProvider)
+                        }
+                        imageCapture = ImageCapture.Builder().build()
 
                         val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
                         try {
@@ -69,7 +82,8 @@ fun CameraScreen(modifier: Modifier = Modifier, navController: NavController) {
                             cameraProvider.bindToLifecycle(
                                 lifecycleOwner,
                                 cameraSelector,
-                                preview
+                                preview,
+                                imageCapture
                             )
                         } catch (e: Exception) {
                             Log.e("CameraPreview", "Error starting camera", e)
@@ -79,6 +93,36 @@ fun CameraScreen(modifier: Modifier = Modifier, navController: NavController) {
                 },
                 modifier = Modifier.weight(1f)
             )
+            if(!isPhotoTaken){
+                Button(onClick = {
+                    imageCapture?.let { capture ->
+                        capture.takePicture(
+                            cameraExecutor,
+                            object : ImageCapture.OnImageCapturedCallback() {
+                                override fun onCaptureSuccess(image: ImageProxy) {
+                                    super.onCaptureSuccess(image)
+                                    isPhotoTaken = true
+
+                                }
+
+                                override fun onError(exception: ImageCaptureException) {
+                                    super.onError(exception)
+                                    Log.e("Camera", "error: ${exception.message}")
+                                }
+                            }
+                        )
+                    }
+
+                }) {
+                    Text(text = "Take Picture")
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+            }else{
+                Button(onClick = { isPhotoTaken = false }) {
+                    Text(text = "OK")
+                }
+                Spacer(modifier = Modifier.height(16.dp))
+            }
             Button(onClick = { navController.navigateUp() }) {
                 Text(text = "Back to Main")
             }
